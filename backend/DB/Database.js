d76a4397ba30c184ee77e549d8d82b613543a88c
MadCoder7 <shankarpubg4@gmail.com>
2025-08-11 13:24:24 +0530

    Backend Updated

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -5,7 +5,7 @@ dotenv.config();
 export async function connectDB() {
   try {
     mongoose.connection.on("connected", () => console.log("DB Connected"));
-    await mongoose.connect(process.env.MONGODB + "/pingUP", {});
+    await mongoose.connect(process.env.MONGODB, {});
   } catch (error) {
     console.error(" DB Connection Error:", error.message);
     process.exit(1);
